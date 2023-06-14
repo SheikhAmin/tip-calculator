a39f9f64aa1ee54127a53e38e8c437f25968b9de
MD.SHEIKH AMIN <sheikhamin.cs@gmail.com>
2023-06-14 16:02:08 +0600

    Js code writing almost done

--- script.js
+++ script.js
@@ -12,4 +12,23 @@ function caculateTip(){
     let noOfPeople = document.getElementById("no-of-people").value;
 
     billInput.value = bill.toFixed(2);
-}
+
+    let totalTip = parseFloat((bill * (tipPercent/100)).toFixed(2));
+    total = parseFloat((bill + totalTip).toFixed(2));
+
+    let tipPerPerson = (totalTip / noOfPeople).toFixed(2);
+    let tottalPerPerson = (total / noOfPeople).toFixed(2);
+
+    document.getElementById("tip-ammount").textContent = `\$ ${totalTip}`;
+    document.getElementById("total-ammount").textContent = `\$ ${total}`;
+
+    document.getElementById("tip-percent").textContent = `\$ ${tipPercent}`;
+    document.getElementById("split-num").textContent = noOfPeople;
+
+    document.getElementById("tip-per-person").textContent = `\$ ${tipPerPerson}`;
+    document.getElementById("total-per-person").textContent = `\$ ${totalPerPerson}`;
+    
+
+} 
+caculateTip();
+
